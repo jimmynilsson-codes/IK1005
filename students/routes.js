a9266428113236653d399f9a43f1f260fd89a3bb
Jimmy Nilsson <nilssonjimmy1987@gmail.com>
2020-02-07 13:40:55 +0100

    removed whitespace

--- students/routes.js
+++ students/routes.js
@@ -8,24 +8,24 @@ routes.get('/', (req, res) => {
 routes.get('/getstudents/', async (req, res) => {
     try {
         const students = await db.getStudents();
+
         res.json(students);
 
     } catch (error) {
         res.json(error);
     }
-
 });
 
 routes.get('/getstudent/:id', async (req, res) => {
     try {
         student_id = req.params.id;
         const student = await db.getStudent(student_id);
+
         res.json(student);
 
     } catch (error) {
         res.json(error);
     }
-
 });
 
 routes.post('/addstudent/', async (req, res) => {
@@ -35,6 +35,7 @@ routes.post('/addstudent/', async (req, res) => {
         const addedStudent = await db.addStudent(studentData);
 
         res.json({ 'Added student': addedStudent });
+        
     } catch (error) {
         res.json(error);
     }
@@ -48,7 +49,9 @@ routes.put('/putstudent/:id', async (req, res) => {
 
     try {
         const updatedStudent = await db.updateStudent(firstname, lastname, age, studentId);
+
         res.json({ 'Old student information': updatedStudent[0], 'New student information': updatedStudent[1] });
+
     } catch (error) {
         res.json(error)
     }
@@ -61,6 +64,7 @@ routes.delete('/delstudent/:id', async (req, res) => {
         const deletedStudent = await db.deleteStudent(studentId);
 
         res.json({ 'Deleted student': deletedStudent });
+
     } catch (error) {
         res.json(error);
     }
