f943b2b548e0b8a2c5c190018dbb9a9e5cee9243
Jimmy Nilsson <nilssonjimmy1987@gmail.com>
2020-02-18 10:57:03 +0100

    added login.js, first push

---
+++ h06jimni_lab/login.js
@@ -0,0 +1,21 @@
+const sqlite = require('sqlite');
+const Promise = require('bluebird');
+
+const dbCon = sqlite.open('./h06jimni_db.db', { Promise });
+
+const checkLogin = async (email) => {
+    try {
+        const db = await dbCon;
+        const selectUserQuery = 'SELECT password FROM users WHERE email = ?';
+
+        const password = await db.get(selectUserQuery, email);
+        return password;
+    } catch (error) {
+        throw error;
+    }
+
+};
+
+module.exports = {
+    checkLogin: checkLogin,
+}
